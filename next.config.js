b4a2fe383d103f7d35534708e4c53e88a6c022e0
Tchasinga jacques <tchasingajacques@gmail.com>
2023-12-29 00:20:46 +0200

    new updating is added

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     distDir: '.next',
+    images: {
+        domains: ['firebasestorage.googleapis.com'],
+    },
 }
 
 module.exports = nextConfig

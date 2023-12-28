891136cf73f3c966350b7910c06f68493e8bd712
Tchasinga jacques <tchasingajacques@gmail.com>
2023-12-28 12:08:58 +0200

    new commit as added

--- src/app/cardata/CardTickects.js
+++ src/app/cardata/CardTickects.js
@@ -1,33 +1,37 @@
-import React from 'react'
-
+import React from "react";
+import Link from "next/link";
 
 async function getCard() {
-  const res = await fetch('https://public-api-information.onrender.com/api/all', {
-    next: {
-      revalidate: 0 // use 0 to opt out of using cache
+  const res = await fetch(
+    "https://public-api-information.onrender.com/api/all",
+    {
+      next: {
+        revalidate: 0, // use 0 to opt out of using cache
+      },
     }
-  })
+  );
 
-  return res.json()
+  return res.json();
 }
 
-
 export default async function CardTickects() {
-    const card = await getCard()
+  const card = await getCard();
   return (
-    <div className='max-w-5xl mx-auto'>
-         {card.map((cards) => (
+    <div className="max-w-5xl mx-auto">
+      {card.map((cards) => (
         <div key={cards._id} className="card my-5">
+          <Link href={`/cardata/${cards._id}`}>
             <h3>{cards.title}</h3>
             <p>{cards.body.slice(0, 200)}...</p>
             <div className={`pill ${cards.priority}`}>
               {cards.priority} priority
             </div>
+          </Link>
         </div>
       ))}
       {card.length === 0 && (
         <p className="text-center">There are no open tickets, yay!</p>
       )}
     </div>
-  )
+  );
 }
---
+++ src/app/cardata/[id]/page.js
@@ -0,0 +1,43 @@
+import React from "react";
+
+async function getCard(_id) {
+  try {
+    await new Promise((resolve) => setTimeout(resolve, 5000));
+    const res = await fetch(
+      `https://public-api-information.onrender.com/api/getId/${_id}`,
+      {
+        next: {
+          revalidate: 0, // use 0 to opt out of using cache which means that refetch data
+        },
+      }
+    );
+    return res.json();
+  } catch (error) {
+    console.error("Error fetching ticket:", error);
+    throw error; // Re-throw the error to propagate it to the caller
+  }
+}
+
+export default async function Carddetails({params}) {
+    const ticket = await getCard(params.id)
+
+  return (
+    <main>
+      <nav>
+        <h2>Ticket Details</h2>
+      </nav>
+      {ticket ? (
+        <div className="card">
+          <h3>{ticket.title}</h3>
+          <small>Created by {ticket.user_email}</small>
+          <p>{ticket.body}</p>
+          <div className={`pill ${ticket.priority}`}>
+            {ticket.priority} priority
+          </div>
+        </div>
+      ) : (
+        <p>Loading...</p>
+      )}
+    </main>
+  );
+}

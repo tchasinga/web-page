e32f838d098bda87bb836cc24afef185e09abf06
Tchasinga jacques <tchasingajacques@gmail.com>
2023-12-28 13:11:41 +0200

    new coding

--- src/app/page.js
+++ src/app/page.js
@@ -5,11 +5,11 @@ export default function Home() {
     <main>
       <h2>Dashboard</h2>
       <p>
-        Let's take a brief look at some of the topics that we chose to omit from
+        Let&apos;s take a brief look at some of the topics that we chose to omit from
         this book, but which you may find in some other introductory JavaScript
         material. Bear in mind that we cover all of these topics in depth in our
-        Core Curriculum at Launch School, so we don't ignore them. However,
-        there's a time and a place where a novice programmer is ready to learn
+        Core Curriculum at Launch School, so we don&apos;t ignore them. However,
+        there&apos;s a time and a place where a novice programmer is ready to learn
         about certain topics. Historically, JavaScript was inextricably tied to
         the DOM (Document Object Model) API, which is what you use to manipulate
         content on a webpage.
@@ -26,7 +26,7 @@ export default function Home() {
       <div className="card">
         <h3>New member of the web dev team...</h3>
         <p>
-          The History API provides access to the browser's session history (not
+          The History API provides access to the browser&apos;s session history (not
           to be confused with WebExtensions history) through the history global
           object. It exposes useful methods and properties that let you navigate
           back and forth through the user's history, and manipulate the contents
@@ -37,8 +37,8 @@ export default function Home() {
         <h3>New website live!</h3>
         <p>
           Hello, I have to send an array as parameter (the API only supports the
-          said parameter as array) but I don’t know how to do it. I managed make
-          it work in Postman but it doesn’t in UiPath. There are two arrays
+          said parameter as array) but I don&apos;t know how to do it. I managed make
+          it work in Postman but it doesn&apos;t in UiPath. There are two arrays
           actually, one for sender and one for receiver. Each array has to
           contain the following: name: string, contact: string, phone1: string,
           locality_id: integer, street: string.
